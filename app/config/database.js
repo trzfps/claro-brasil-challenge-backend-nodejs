970c5c2f166130ecd126c890637deb58fe1f52ce
vitor <vitor_48@hotmail.com>
2019-05-29 01:38:40 -0300

    adding procFIle to deploy application in heroku and change ambient variable of database

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb://${process.env.DB_HOST}/devices`, {
+mongoose.connect(`${process.env.DB_HOST}`, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
